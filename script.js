5fd8ebb19d67f72738c18b1848665fabe8869fb0
FelipeLuis24021995 <132674086+FelipeLuis24021995@users.noreply.github.com>
2023-06-08 09:02:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function imprimeArray() {
+    var ganhos99 = parseFloat(document.getElementById('caixa1').value);
+    var ganhosUber = parseFloat(document.getElementById('caixa2').value);
+    var ganhosIndrav = parseFloat(document.getElementById('caixa3').value);
+    var ganhosParti = document.getElementById('caixa4').value;
+    var ganhosParticular = eval(ganhosParti);
+    var kmRodados = parseFloat(document.getElementById('caixa5').value);
+    var horasTrabalhadas = parseFloat(document.getElementById('caixa6').value);
+
+    var ganhos = ganhos99 + ganhosUber + ganhosIndrav + ganhosParticular;  
+    var ganhosPorKM = ganhos / kmRodados;
+    var ganhosPorHora = ganhos / horasTrabalhadas;
+  
+    var result = 
+      "Ganhos 99: " + ganhos99 + " Reais." + "<br>"+
+      "Ganhos Uber: " + ganhosUber + " Reais." + "<br>"+
+      "Ganhos Indrav: " + ganhosIndrav + " Reais." + "<br>"+   
+      "Ganhos Particular: " + ganhosParticular + " Reais." + "<br>"+  
+      "Com " + kmRodados + " KM rodados." + "<br>"+
+      "Em " + horasTrabalhadas + " horas trabalhadas." + "<br>"+
+      "Total de ganhos do dia: " + ganhos + " Reais." + "<br>"+
+      "Ganhos por KM: " + ganhosPorKM + " Reais por KM rodado." + "<br>"+
+      "Ganhos por hora: " + ganhosPorHora + " Reais por hora.";
+  
+    document.getElementById('resultado').innerHTML = result;
+    
+    document.getElementById('resu').classList.remove("hide") 
+}
+
+function voltar() {
+  document.getElementById('resu').classList.add("hide")
+}
